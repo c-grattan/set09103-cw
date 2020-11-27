87680726b7d9f2f201b1dfa06d038c5ad4aa3c05
c-grattan <56635397+c-grattan@users.noreply.github.com>
2020-11-27 16:48:41 +0000

    Fix template rendering

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template("templates/index.html"), 200
+	return render_template("index.html"), 200
 
 if __name__ == "__main__":
     app.run()
