4ac2226e5c9fc07334ab4db5d50759a52c95f502
c-grattan <56635397+c-grattan@users.noreply.github.com>
2020-11-27 16:44:51 +0000

    Update app.py
    Now returns a template for the index

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Index', 200
+	return render_template("templates/index.html"), 200
 
 if __name__ == "__main__":
     app.run()
