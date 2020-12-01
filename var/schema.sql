9797a829af3ab0531729af747ee32b3ccbcec4a5
c-grattan <56635397+c-grattan@users.noreply.github.com>
2020-12-01 15:45:49 +0000

    Added new predefined equation, no starting custom equations

--- var/schema.sql
+++ var/schema.sql
@@ -13,8 +13,6 @@ CREATE TABLE equations (
 	user integer
 );
 
-INSERT INTO equations VALUES("The Placeholder Theorum","y=mx+c", -1);
-INSERT INTO equations VALUES("The Sample Quandary","x^2-1", -1);
-
-INSERT INTO equations VALUES("The Custom Theorum","y=mx+c+q", 0);
-INSERT INTO equations VALUES("The Qustom Quandary","x^2-1+q", 0);
+INSERT INTO equations VALUES("The Placeholder Theorum","mx+c", -1);
+INSERT INTO equations VALUES("The Sample Quandary","x**2-1", -1);
+INSERT INTO equations VALUES("Theory of Special Relativity","mc**2", -1);
